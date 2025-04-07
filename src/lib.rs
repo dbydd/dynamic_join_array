49ccd2f8bba8efcbe1ba3c2b17298aacdad58115
dbydd <1992003927@qq.com>
2025-04-07 21:42:58 +0800

    make it more reasonable

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,52 @@
 #![no_std]
-#![feature(allocator_api)]
+#![feature(allocator_api, sync_unsafe_cell, ptr_as_ref_unchecked)]
 
-use core::{future::Future, pin::Pin, task::Poll};
+use core::{
+    cell::{SyncUnsafeCell, UnsafeCell},
+    future::Future,
+    ops::AddAssign,
+    pin::Pin,
+    task::Poll,
+};
 
 use alloc::{boxed::Box, collections::btree_map::BTreeMap, vec::Vec};
+use async_lock::RwLock;
 
 extern crate alloc;
-pub trait NoEndFuture: Future<Output = ()> + Unpin {}
+pub trait NoEndFuture: Future<Output = ()> + Send + Sync {}
 
 pub struct DynamicJoinArray {
-    job_queue: BTreeMap<usize, Box<dyn NoEndFuture>>,
-    cycle_counter: usize,
+    job_queue: RwLock<BTreeMap<usize, Pin<Box<dyn NoEndFuture>>>>,
+    cycle_counter: RwLock<usize>,
 }
 
 impl DynamicJoinArray {
     pub fn new() -> Self {
         Self {
-            job_queue: BTreeMap::new(),
-            cycle_counter: 0,
+            job_queue: BTreeMap::new().into(),
+            cycle_counter: 0.into(),
         }
     }
 
-    pub fn add(&mut self, fut: Box<dyn NoEndFuture>) -> usize {
-        self.job_queue.insert(self.cycle_counter, fut);
-        let id = self.cycle_counter;
-        self.cycle_counter += 1;
+    pub async fn add(&self, fut: Pin<Box<dyn NoEndFuture>>) -> usize {
+        let mut guard = self.cycle_counter.write().await;
+        let id = *guard;
+        guard.add_assign(1);
+        self.job_queue.write().await.insert(id, fut);
         id
     }
 
-    pub fn drop(&mut self, id: &usize) -> Option<Box<dyn NoEndFuture>> {
-        self.job_queue.remove(id)
+    pub async fn drop(&self, id: &usize) -> Option<Pin<Box<dyn NoEndFuture>>> {
+        self.job_queue.write().await.remove(id)
     }
 
-    pub fn work<'a>(&'a mut self) -> DynamicJoinArrayFuture<'a> {
+    pub fn work<'a>(&'a self) -> DynamicJoinArrayFuture<'a> {
         DynamicJoinArrayFuture { ref_to_array: self }
     }
 }
 
 pub struct DynamicJoinArrayFuture<'a> {
-    ref_to_array: &'a mut DynamicJoinArray,
+    ref_to_array: &'a DynamicJoinArray,
 }
 
 impl<'a> Future for DynamicJoinArrayFuture<'a> {
@@ -49,21 +57,25 @@ impl<'a> Future for DynamicJoinArrayFuture<'a> {
         cx: &mut core::task::Context<'_>,
     ) -> core::task::Poll<Self::Output> {
         let this = unsafe { self.get_unchecked_mut() };
-        this.ref_to_array
-            .job_queue
-            .iter_mut()
-            .filter_map(
-                |(id, job)| match unsafe { Pin::new_unchecked(job).poll(cx) } {
-                    core::task::Poll::Ready(_) => Some(id.clone()),
-                    core::task::Poll::Pending => None,
-                },
-            )
-            .collect::<Vec<_>>()
-            .iter()
-            .for_each(|to_drop| {
-                this.ref_to_array.job_queue.remove(to_drop);
-            });
-
+        match this.ref_to_array.job_queue.try_write() {
+            Some(mut queue_ref) => {
+                let to_drop = queue_ref
+                    .iter_mut()
+                    .filter_map(
+                        |(id, job)| match unsafe { Pin::new_unchecked(job).poll(cx) } {
+                            core::task::Poll::Ready(_) => Some(id.clone()),
+                            core::task::Poll::Pending => None,
+                        },
+                    )
+                    .collect::<Vec<_>>();
+                if !to_drop.is_empty() {
+                    to_drop.iter().for_each(|to_drop_e| {
+                        queue_ref.remove(to_drop_e);
+                    });
+                }
+            }
+            None => {}
+        }
         Poll::Pending
     }
 }
@@ -87,19 +99,19 @@ mod tests {
     impl Unpin for DummyFuture {}
     impl NoEndFuture for DummyFuture {}
 
-    #[test]
-    fn test_dynamic_join_array() {
+    #[tokio::test]
+    async fn test_dynamic_join_array() {
         // Create a new DynamicJoinArray
-        let mut dynamic_join_array = DynamicJoinArray {
-            job_queue: BTreeMap::new(),
-            cycle_counter: 0,
-        };
+        let mut dynamic_join_array = DynamicJoinArray::new();
 
         // Add a dummy future
-        let future_id = dynamic_join_array.add(Box::new(DummyFuture));
+        let future_id = dynamic_join_array.add(Box::pin(DummyFuture)).await;
 
         // Check that the future has been added to the job queue
-        assert!(dynamic_join_array.job_queue.contains_key(&future_id));
+        assert!(dynamic_join_array
+            .job_queue
+            .get_mut()
+            .contains_key(&future_id));
 
         // Simulate polling the future (this would normally happen during runtime)
         let mut context = core::task::Context::from_waker(futures::task::noop_waker_ref());
@@ -114,12 +126,18 @@ mod tests {
         }
 
         // After polling, the future should be removed from the job queue
-        assert!(!dynamic_join_array.job_queue.contains_key(&future_id));
+        assert!(!dynamic_join_array
+            .job_queue
+            .get_mut()
+            .contains_key(&future_id));
 
         // Drop the future explicitly
         dynamic_join_array.drop(&future_id);
 
         // Ensure that the future is no longer in the job queue
-        assert!(!dynamic_join_array.job_queue.contains_key(&future_id));
+        assert!(!dynamic_join_array
+            .job_queue
+            .get_mut()
+            .contains_key(&future_id));
     }
 }
